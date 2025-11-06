658f44579ff8947b3fca5a644770a47e2a2496c8
Pramod Palde <pramodpalde111@gmail.com>
2025-11-06 08:53:34 +0530

    updated the app.jsx and card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
   return (
     <div >
         <div className='card'>
-            <img src='https://images.unsplash.com/photo-1761991062157-df67faab3a23?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1074'/>
+            <img src={props.img}/>
             <h1>{props.name}</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, numquam.</p>
             <button className='btn'>veiw Profile</button>
